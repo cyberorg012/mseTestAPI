547d6d1aa35ee169c1cb422ed0c5ee2e59410ca2
Hyewon Park <changeling626@gmail.com>
2019-07-06 01:35:09 +0900

    hyewon: router files prepared;

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const db = require('./database/config');
 
 app.use('/api/user', require('./router/user').user);
+app.use('/api/board', require('./router/board').board);
+app.use('/api/board_comment', require('./router/board_comment').board_comment);
+app.use('/api/gallery', require('./router/gallery').gallery);
+app.use('/api/gallery_comment', require('./router/gallery_comment').gallery_comment);
+app.use('/api/schedule', require('./router/schedule').schedule);
 
 app.listen(3000, () => {
 	console.log('Your API Listening on Port 3000!');
---
+++ router/gallery_comment.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser')
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+const db = require('../database/config');
+
+router.get('/', (req, res) => { // retrieve every rows
+	db.then(client => {
+		client.query("select * from gallery_comment", (err, rows) => {
+			if (!err) {
+				// console.log(rows);
+				return res.json(rows);
+			} else {
+				console.log(`query error : ${err}`);
+				return res.status(400).json({error: "Retrieve Error"});
+			}
+		});
+	})
+})
+
+module.exports.gallery_comment = router;
