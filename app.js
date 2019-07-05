7ea38e040f6efb5b73a8e0892df2792066d4d19a
Hyewon Park <changeling626@gmail.com>
2019-07-06 01:27:28 +0900

    hyewon: directory arranged;

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const express = require('express');
 const app = express();
-const db = require('./config');
+const db = require('./database/config');
 
-app.listen(3001, () => {
-	console.log('Example app listening on port 3001!');
+app.use('/api/user', require('./router/user').user);
+
+app.listen(3000, () => {
+	console.log('Your API Listening on Port 3000!');
 });
 
-app.get('/welcome', (req, res) => { // root page
+app.get('/api/welcome', (req, res) => { // root page
 	res.send('Welcome to MSE Testing API!\n');
 });
-app.get('/', (req, res) => { // retrieve every rows
-	db.then(client => {
-		client.query("select * from user", (err, rows) => {
-			if (!err) {
-				// console.log(rows);
-				return res.json(rows);
-			} else {
-				console.log(`query error : ${err}`);
-				return res.status(400).json({error: "Retrieve Error"});
-			}
-		});
-	})
-})
 
 module.exports = app;
