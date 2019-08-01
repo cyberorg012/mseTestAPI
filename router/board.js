466215e5a5e4a37fd32bcbf4bc629575454a38f8
하승아 <hsa20130614@gmail.com>
2019-08-01 18:25:46 +0900

    board.js multer added, post img router added

--- router/board.js
+++ router/board.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path')
+const multer = require('multer')
 const router = express.Router();
 const bodyParser = require('body-parser')
 router.use(bodyParser.json());
@@ -43,6 +45,26 @@ router.get('/category/:category', (req, res) => { // retrieve every rows
 			}
 		});
 	})
+})
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads/')
+	},
+	filename: (req, file, cb) => {
+		const ext = path.extname(file.originalname)
+		cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext)
+	}
+})
+const upload = multer({
+	storage: storage,
+	limits: 10 * 1024 * 1024
+})
+router.post('/images', upload.array('images'), (req, res, next) => {
+	let url = []
+	for(var i=0; i<req.files.length; i++) {
+		url[i] = `//${process.env.addr}/img/${req.files[i].filename}`
+	}
+	res.json({ url: url})
 })
 
 module.exports.board = router;
